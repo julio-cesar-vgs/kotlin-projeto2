3f36b380fbd4a800149903119f90498e3ca134c7
Sleepk <julio.cesar.vgs@gmail.com>
2019-09-23 00:14:16 -0300

    Projeto motivation

---
+++ Motivation/app/src/main/java/devmasterteam/com/Splashctivity.kt
@@ -0,0 +1,12 @@
+package devmasterteam.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splashctivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashctivity)
+    }
+}
