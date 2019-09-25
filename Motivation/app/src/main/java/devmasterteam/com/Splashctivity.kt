c11d5fc4ce01a26642940041a3792b1fec2b37b3
Julio Cesar <julio.cesar.vgs@gmail.com>
2019-09-24 23:29:48 -0300

    Aula - 117. Navegacao entre activity
    Signed-off-by: Sleepk <julio.cesar.vgs@gmail.com>

--- Motivation/app/src/main/java/devmasterteam/com/Splashctivity.kt
+++ Motivation/app/src/main/java/devmasterteam/com/Splashctivity.kt
@@ -1,12 +1,50 @@
 package devmasterteam.com
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import devmasterteam.com.util.MotivationConstants
+import devmasterteam.com.util.SecurityPreferences
+import kotlinx.android.synthetic.main.activity_splashctivity.*
 
-class Splashctivity : AppCompatActivity() {
+class Splashctivity : AppCompatActivity(), View.OnClickListener {
+
+    // instancia de uma SecurityPreferences com carregamento atrasado
+    private lateinit var mSecurity: SecurityPreferences
+
+    // funcao para o click
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.buttonSave) {
+            handleSave()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashctivity)
+
+        mSecurity = SecurityPreferences(this)
+
+        buttonSave.setOnClickListener(this)
+    }
+
+
+    // funcao que fara o save das informacoes.
+    private fun handleSave() {
+        val name = editName.text.toString()
+
+        if (name.isNullOrEmpty()) {
+            Toast.makeText(this, "Por favor informe seu nome", Toast.LENGTH_LONG).show()
+        } else {
+            mSecurity.storeString(MotivationConstants.KEY.PERSON_NAME, name)
+
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+
+        }
     }
 }
---
+++ Motivation/app/src/main/java/devmasterteam/com/util/SecurityPreferences.kt
@@ -0,0 +1,28 @@
+package devmasterteam.com.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+
+/**
+ * Classe responsavel por guardar as informacoes do usuario
+ * @param passando um conexto como parametro
+ */
+class SecurityPreferences(context: Context) {
+
+    // instancia que vai guardar uma chave e um formato para ser chamado.
+    private val mSharedPreferences: SharedPreferences = context
+        .getSharedPreferences("motivation", Context.MODE_PRIVATE)
+
+
+    //funcao que vai guardar as informacoes que vamos passar, por uma chave  e valor
+    fun storeString(key: String, value: String) {
+        mSharedPreferences.edit().putString(key, value).apply()
+    }
+
+
+    // funcao que vai recuperar as informacoes passadas para o usuario
+    fun getStoredString(key: String, value: String?): String {
+        return mSharedPreferences.getString(key, "") ?: ""
+    }
+}
