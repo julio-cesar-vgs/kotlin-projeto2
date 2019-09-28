ae6a0c5d9e3649b2805393570f09678431587926
Julio Cesar <julio.cesar.vgs@gmail.com>
2019-09-28 10:04:20 -0300

    Manipulacao da constraint layout
    Signed-off-by: Julio Souza <julio.cesar.vgs@gmail.com>

---
+++ Motivation/app/src/main/java/devmasterteam/com/views/SpotActivity.kt
@@ -0,0 +1,13 @@
+package devmasterteam.com.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import devmasterteam.com.R
+
+class SpotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spot)
+    }
+}
